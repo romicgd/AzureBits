d6a78471d8f65da8252c3febe38c823c46748ca5
c3.Roman.Guoussev-Do <roman.guoussev-donskoi@ontario.ca>
2018-09-24 19:27:52 -0400

    cleanup

--- Compute/Report/get-VM-sizes.ps1
+++ Compute/Report/get-VM-sizes.ps1
@@ -2,20 +2,5 @@ get-Azurermvm  | select @{N='vmSize';E={$_.HardwareProfile.VMsize}},
 @{N='OS'; E={$_.storageProfile.OSDisk.OSType}} | group -property vmsize, OS | select count, name | sort name
 
 
-$vmsizearr = get-Azurermvm  
-
-foreach ($vm in $vmsizearr) {
-    $vmsize=Get-AzureRmVMSize -resourcegroupname $vm.resourcegroupname -vmname $vm.name | where-object {$_.name -eq $($vm.HardwareProfile.VMsize)} 
-    $nics = Get-AzureRmNetworkInterface 
-    $vmIPs = @()
-    Foreach($nic in $nics)  {
-        if($vm.Id -eq $nic.virtualmachine.id) {
-            $vmIP = $( Get-AzureRmNetworkInterfaceIpConfig -NetworkInterface $nic | Select PrivateIPAddress) 
-            $vmIPs += $vmIp.PrivateIPaddress
-        }
-    }
-    $vmIPStr=$vmIPs -join ','    
-    "$($vm.name) - $($vm.HardwareProfile.VMsize): CPU[$($vmsize.NumberOfCores)], RAM[$($vmsize.MemoryInMB/1024)], Disk[$($vmsize.OSDiskSizeInMB/1024)], IP[$vmIPStr]" 
-}    
 
 
